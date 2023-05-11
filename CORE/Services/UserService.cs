f14daef8930e9a284e80544990c747175628de90
Svaaan <daniel_svan1@hotmail.com>
2023-05-11 23:46:45 +0200

    frontpage changes

--- CORE/Services/UserService.cs
+++ CORE/Services/UserService.cs
@@ -3,29 +3,12 @@
 
 // namespace CORE.Services;
 
-<<<<<<< HEAD
 // public class UserService : Iservice<CORE.Entities.User>
 // {
 //     public async Task<User> add(User obj)
-// <<<<<<< HEAD
-//     private readonly Iservices _iServices;
-
-//     public UserService(Iservices iServices)
-//     {
-//         _iServices = iServices;
-// =======
-//     public User add(User obj)
 //     {
 //         throw new NotImplementedException();
 //     }
-=======
-public class UserService : Iservice<CORE.Entities.User>
-{
-    public async Task<User> add(User obj)
-    {
-        throw new NotImplementedException();
-    }
->>>>>>> 25b94b6e2c0afb3fb67f9bb3c3cdd7c224a6a44a
 
 //     public async Task<User> Delete(User obj)
 //     {
