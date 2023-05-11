c184ea1badb9f36dc57bb3df762792f20faa703a
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-05-12 00:37:35 +0200

    commit

--- INFRASTRUCTURE/APIControllers/Challengecontroller.cs
+++ INFRASTRUCTURE/APIControllers/Challengecontroller.cs
@@ -1,75 +1,75 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using CORE.Entities;
-using CORE.Services;
-using Microsoft.Extensions.Logging;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Mvc;
+// using CORE.Entities;
+// using CORE.Services;
+// using Microsoft.Extensions.Logging;
 
-namespace INFRASTRUCTURE.APIControllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class Challangecontroller : ControllerBase
-    {
-        private readonly ILogger<Challangecontroller> _logger;
-        private readonly Challenge _challenge;
-        private readonly User _user;
+// namespace INFRASTRUCTURE.APIControllers
+// {
+//     [ApiController]
+//     [Route("api/[controller]")]
+//     public class Challangecontroller : ControllerBase
+//     {
+//         private readonly ILogger<Challangecontroller> _logger;
+//         private readonly Challenge _challenge;
+//         private readonly User _user;
 
-        public Challangecontroller(ILogger<Challangecontroller> logger, Challenge challenge, User user)
-        {
-            _logger = logger;
-            _challenge = challenge;
-            _user = user;
-        }
+//         public Challangecontroller(ILogger<Challangecontroller> logger, Challenge challenge, User user)
+//         {
+//             _logger = logger;
+//             _challenge = challenge;
+//             _user = user;
+//         }
 
-        [HttpPost]
-        public async Task<ActionResult<Challenge>> CreateUserChallenge([FromBody] Challenge challenge)
-        {
-            var createChallenge = await this._challenge.CreateChallenge(challenge);
+//         [HttpPost]
+//         public async Task<ActionResult<Challenge>> CreateUserChallenge([FromBody] Challenge challenge)
+//         {
+//             var createChallenge = await this._challenge.CreateChallenge(challenge);
 
-            if (createChallenge == null)
-            {
-                return BadRequest();
-            }
+//             if (createChallenge == null)
+//             {
+//                 return BadRequest();
+//             }
 
-            return Ok();
-        }
+//             return Ok();
+//         }
 
-        [HttpPost]
-        public async Task<ActionResult<User>> UserChallenge([FromBody] User user)
-        {
-            var userChallenge = await this.UserToUserChallenge(user);
+//         [HttpPost]
+//         public async Task<ActionResult<User>> UserChallenge([FromBody] User user)
+//         {
+//             var userChallenge = await this.UserToUserChallenge(user);
 
-            if (userChallenge == null)
-            {
-                return BadRequest();
-            }
+//             if (userChallenge == null)
+//             {
+//                 return BadRequest();
+//             }
 
-            return Ok();
-        }
+//             return Ok();
+//         }
 
-        [HttpGet]
-        public async Task<ActionResult<List<Challenge>>> GetAllChallanges()
-        {
-            var challangeList = await this._challenge.ChallangesList();
-            if (challangeList == null)
-            {
-                return BadRequest();
-            }
-            return Ok();
-        }
+//         [HttpGet]
+//         public async Task<ActionResult<List<Challenge>>> GetAllChallanges()
+//         {
+//             var challangeList = await this._challenge.ChallangesList();
+//             if (challangeList == null)
+//             {
+//                 return BadRequest();
+//             }
+//             return Ok();
+//         }
 
-        [HttpGet("{int:id}")]
-        public async Task<ActionResult<Challenge>> GetChallengeById(int id)
-        {
-            var result = await this._challenge.ChallengeById(id);
-            if (result == null)
-            {
-                return NotFound();
-            }
-            return Ok();
-        }
-    }
-}
+//         [HttpGet("{int:id}")]
+//         public async Task<ActionResult<Challenge>> GetChallengeById(int id)
+//         {
+//             var result = await this._challenge.ChallengeById(id);
+//             if (result == null)
+//             {
+//                 return NotFound();
+//             }
+//             return Ok();
+//         }
+//     }
+// }
