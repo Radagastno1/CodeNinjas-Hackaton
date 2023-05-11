f87a00d868b92ce403ec9442d88e101151125bbd
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-05-11 17:23:43 +0200

    packages to infrastructure

---
+++ INFRASTRUCTURE/API-Controllers/Challangecontroller.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CORE.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace INFRASTRUCTURE.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Challangecontroller : ControllerBase
+    {
+        private readonly ILogger<Challangecontroller> _logger;
+        private readonly Challenge _chellange;
+
+        public Challangecontroller(ILogger<Challangecontroller> logger, Challenge challenge)
+        {
+            _logger = logger;
+            _chellange = challenge;
+        }
+
+        public IActionResult<Challenge> CreateNewChallenge([FromBody] Challenge challenge)
+        {
+
+        }
+    }
+}
