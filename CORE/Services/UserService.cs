1fda56f3e9d4311a33e3a6b79c2f26481c64ae48
Elina Kerola <elinak90@icloud.com>
2023-05-11 19:30:47 +0200

    new using

--- CORE/Services/UserService.cs
+++ CORE/Services/UserService.cs
@@ -1,6 +1,13 @@
+using CORE.Entities;
+using CORE.Services;
 namespace CORE.Services;
 
-public class UserService
+public class UserService : interfaces.Services<CORE.Entities.User>
 {
-    
+    private readonly Iservices _iServices;
+
+    public UserService(Iservices iServices)
+    {
+        _iServices = iServices;
+    }
 }
