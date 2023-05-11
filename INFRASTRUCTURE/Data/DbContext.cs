de162730a149258cfdaa7f56e7568aef895f8798
Elin Nyman <elin.nyman87@gmail.com>
2023-05-11 17:41:05 +0200

    DbContext created

---
+++ INFRASTRUCTURE/Data/DbContext.cs
@@ -0,0 +1,18 @@
+#nullable disable
+
+namespace INFRASTRUCTURE.Data;
+
+public class DbContext : DbContext
+{
+    public BioTrananDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Movie>().HasData(new Movie
+        {
+        });
+    }
+
+    public DbSet<Movie> Movies { get; set; }
+}
