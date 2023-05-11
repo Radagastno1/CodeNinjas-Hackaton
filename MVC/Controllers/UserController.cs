272e9e8a332790d6d50b217549de9779bc572871
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-05-11 23:42:21 +0200

    views in mvc

--- MVC/Controllers/UserController.cs
+++ MVC/Controllers/UserController.cs
@@ -1,13 +1,16 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MVC.Models;
+using CORE.Interfaces;
 
 namespace MVC.Controllers;
+
 public class UserController : Controller
 {
     private readonly ILogger<UserController> _logger;
+    private readonly Iservice<CORE.Entities.User> _userService;
 
-    public UserController(ILogger<UserController> logger)
+    public UserController(ILogger<UserController> logger, Iservice<CORE.Entities.User> userService)
     {
         _logger = logger;
     }
@@ -17,4 +20,20 @@ public class UserController : Controller
         return View();
     }
 
+    [HttpPost]
+    public async Task<IActionResult> LogIn(UserViewModel userViewModel)
+    {
+        var user = new CORE.Entities.User { user_inlog = userViewModel.UserInlog };
+
+        var loggedInUser = await _userService.add(user); //här får vi vilka utmaningear som personen har gjort och ej
+
+        userViewModel = new UserViewModel
+        {
+            Id = loggedInUser.userId,
+            Name = loggedInUser.user_name,
+            AvailableChallenges = loggedInUser.challenges.ToList() //här hämta bara de som EJ är gjorda sen
+        };
+
+        return RedirectToAction("Index", "Challenge", userViewModel);
+    }
 }
