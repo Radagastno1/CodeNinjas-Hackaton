8b7817219cbc73c78a6abde8b636e5b944d00b2e
Tricksterone <johan.karlsson089@gmail.com>
2023-05-11 19:17:03 +0200

    challengectrlUpdate

--- INFRASTRUCTURE/API-Controllers/Challangecontroller.cs
+++ INFRASTRUCTURE/API-Controllers/Challangecontroller.cs
@@ -14,44 +14,40 @@ namespace INFRASTRUCTURE.Controller
     public class Challangecontroller : ControllerBase
     {
         private readonly ILogger<Challangecontroller> _logger;
-        private readonly Challenge _chellange;
+        private readonly Challenge _challenge;
         private readonly User _user;
 
         public Challangecontroller(ILogger<Challangecontroller> logger, Challenge challenge, User user)
         {
             _logger = logger;
-            _chellange = challenge;
+            _challenge = challenge;
             _user = user;
         }
 
         [HttpPost]
-        public async Task<IActionResult<Challenge>> CreateUserChallenge([FromBody] Challenge challenge)
+        public async Task<ActionResult<Challenge>> CreateUserChallenge([FromBody] Challenge challenge)
         {
-            var createChallenge = await this._chellange.CreateChallenge(challenge);
+            var createChallenge = await this._challenge.CreateChallenge(challenge);
 
-            if (!ModelState.IsValid)
+            if (createChallenge == null)
             {
-                throw new ArgumentNullException();
                 return BadRequest();
             }
-            return (IActionResult<Challenge>)Ok();
+
+            return Ok();
         }
 
         [HttpPost]
-        public async Task<IActionResult<User>> UserChallenge([FromBody] User user)
+        public async Task<ActionResult<User>> UserChallenge([FromBody] User user)
         {
             var userChallenge = await this.UserToUserChallenge(user);
 
-            if (!ModelState.IsValid)
+            if (userChallenge == null)
             {
-                throw new ArgumentException();
                 return BadRequest();
             }
+
             return Ok();
         }
-    }
-
-    public interface IActionResult<T>
-    {
     }
 }
--- INFRASTRUCTURE/Data/DbContext.cs
+++ INFRASTRUCTURE/Data/DbContext.cs
@@ -1,4 +1,5 @@
-using CORE.Entity;
+using CORE.Entities;
+using Microsoft.EntityFrameworkCore;
 #nullable disable
 
 namespace INFRASTRUCTURE.Data;
