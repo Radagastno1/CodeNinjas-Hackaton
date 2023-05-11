db28e10dd32641d98d7d0149c5947abee3aa95ea
Tricksterone <johan.karlsson089@gmail.com>
2023-05-11 18:27:56 +0200

    challengecontrollerMockup

--- INFRASTRUCTURE/API-Controllers/Challangecontroller.cs
+++ INFRASTRUCTURE/API-Controllers/Challangecontroller.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CORE.Entities;
+using CORE.Services;
 using Microsoft.Extensions.Logging;
 
 namespace INFRASTRUCTURE.Controller
@@ -14,14 +15,17 @@ namespace INFRASTRUCTURE.Controller
     {
         private readonly ILogger<Challangecontroller> _logger;
         private readonly Challenge _chellange;
+        private readonly User _user;
 
-        public Challangecontroller(ILogger<Challangecontroller> logger, Challenge challenge)
+        public Challangecontroller(ILogger<Challangecontroller> logger, Challenge challenge, User user)
         {
             _logger = logger;
             _chellange = challenge;
+            _user = user;
         }
 
-        public async IActionResult<Challenge> CreateUserChallenge([FromBody] Challenge challenge)
+        [HttpPost]
+        public async Task<IActionResult<Challenge>> CreateUserChallenge([FromBody] Challenge challenge)
         {
             var createChallenge = await this._chellange.CreateChallenge(challenge);
 
@@ -30,6 +34,24 @@ namespace INFRASTRUCTURE.Controller
                 throw new ArgumentNullException();
                 return BadRequest();
             }
+            return (IActionResult<Challenge>)Ok();
         }
+
+        [HttpPost]
+        public async Task<IActionResult<User>> UserChallenge([FromBody] User user)
+        {
+            var userChallenge = await this.UserToUserChallenge(user);
+
+            if (!ModelState.IsValid)
+            {
+                throw new ArgumentException();
+                return BadRequest();
+            }
+            return Ok();
+        }
+    }
+
+    public interface IActionResult<T>
+    {
     }
 }
